3f039c279904663944b64cc79c32da6edf966037
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2023-07-25 01:16:55 +0330

    dev-1.0.2 completed

--- config/product/models.py
+++ config/product/models.py
@@ -1,10 +1,27 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
+
+from .fields import OderField
+
+# class ActiveManager(models.Manager):
+# def get_queryset(self):
+#     return super().get_queryset().filter(is_active=True)
+# def isactive(self):
+#     return self.get_queryset().filter(is_active=True)
+
+
+class ActiveQuerySet(models.QuerySet):
+    def isactive(self):
+        return self.filter(is_active=True)
 
 
 class Category(MPTTModel):
     name = models.CharField(max_length=100, unique=True)
     parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
+    is_active = models.BooleanField(default=False)
+    slug = models.SlugField(max_length=255)
+    objects = ActiveQuerySet.as_manager()
 
     class MPTTMeta:
         order_insertion_by = ["name"]
@@ -15,6 +32,8 @@ class Category(MPTTModel):
 
 class Brand(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    is_active = models.BooleanField(default=False)
+    objects = ActiveQuerySet.as_manager()
 
     def __str__(self):
         return self.name
@@ -22,12 +41,47 @@ class Brand(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=255)
     description = models.TextField(blank=True)
     is_digital = models.BooleanField(default=False)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     category = TreeForeignKey(
         "Category", null=True, blank=True, on_delete=models.SET_NULL
     )
+    is_active = models.BooleanField(default=False)
+
+    # objects = models.Manager()
+    # isActive = ActiveManager()
+
+    # objects = ActiveManager()
+
+    objects = ActiveQuerySet.as_manager()
 
     def __str__(self):
         return self.name
+
+
+class ProductLine(models.Model):
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    sku = models.CharField(max_length=100)
+    stock_qty = models.IntegerField()
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="product_line"
+    )
+    is_active = models.BooleanField(default=False)
+    order = models.PositiveIntegerField()
+    objects = ActiveQuerySet.as_manager()
+    order = OderField(unique_for_field="product", blank=True)
+
+    def clean(self, exclude=None):
+        qs = ProductLine.objects.filter(product=self.product)
+        for obj in qs:
+            if self.id != obj.id and self.order == obj.order:
+                raise ValidationError("Duplicate Value.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super(ProductLine, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.sku)
